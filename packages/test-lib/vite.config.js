702545d3bed64af618cd568b6068a3efa090509e
菠萝吹雪 <58333667+yuguaa@users.noreply.github.com>
2024-11-29 10:07:41 +0800

    Initial commit

---
+++ packages/test-lib/vite.config.js
@@ -0,0 +1,38 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import babel from '@rollup/plugin-babel'
+import dts from 'vite-plugin-dts'
+export default defineConfig({
+  plugins: [
+    dts({
+      tsconfigPath: './tsconfig.json',
+      outDir: 'dist/types'
+    })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'testLib',
+      fileName: 'index',
+      formats: ['es', 'amd', 'umd', 'iife', 'cjs', 'esm']
+    },
+    rollupOptions: {
+      plugins: [
+        babel({
+          babelHelpers: 'bundled',
+          extensions: ['.js', '.ts'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: ['defaults', 'not ie <= 11', 'last 2 versions']
+                }
+              }
+            ]
+          ]
+        })
+      ]
+    }
+  }
+})
